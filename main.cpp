7e04ad2c62ae70203bc3aeebcc526e760ad2bf51
mhil-mi <2500018162@webmail.uad.ac.id>
2026-04-09 02:12:57 +0000

    initial commit belajar git pakai c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello Word"
+ << endl;
+cout << "Ini commit pertama belajar git saya" <<endl;
+cout << "Ini commit pertama belajar git saya" <<endl;
+return 0;
+}
